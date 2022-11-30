92352a952f82f84ec063c23ccd58b84fe306d9ed
Iwctwbh <Iwctwbh@gmail.com>
2022-11-30 17:27:01 +0800

    Changes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
@@ -7,7 +7,8 @@ import App from './App.vue'
 
 const app = createApp(App);
 
-app.use(ElementPlus).mount('#app');
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
 }
+
+app.use(ElementPlus).mount('#app');
