a2807079cfc223355c356f3857235e0187e14d4c
Iwctwbh <iwctwbh@gmail.com>
2022-12-01 21:22:28 +0800

    Changes

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@
       <div class="block">
         <div class="demo-range">
           <el-time-picker
-              v-if="timepicker"
               v-model="timepicker"
               :disabled-hours="disabledHours"
               :disabled-minutes="disabledMinutes"
@@ -84,30 +83,34 @@
 </style>
 
 <script setup lang="ts">
-import {ref, watch} from 'vue';
+import {computed, ref, watch} from 'vue';
 import moment from "moment";
-import _ from "lodash";
+import _, {get} from "lodash";
 
 // Init
-let regex_string = new RegExp('');
-let array_textareain: string[] = [];
-
 const textareain = ref('');
 const textregex = ref('');
 const checkbox1 = ref(false);
 const IsLoading = ref(false);
 const textareaout = ref('');
-const datepicker = ref('')
-const timepicker = ref<[Date, Date]>()
-const disabledHours = ref(() => {
-  let array: number[] = [];
-  return array;
-});
+const datepicker = ref('');
+const timepicker = ref<[Date, Date]>();
+const kArrayHours: number[] = Array(24).fill(null).map((item, index) => index);
+const kArrayMinutes: number[] = Array(60).fill(null).map((item, index) => index);
+const kArraySeconds: number[] = Array(60).fill(null).map((item, index) => index);
+
+let regex_string = new RegExp('');
+let array_textareain: string[] = [];
+let array_disabledHours: number[] = [];
+let array_disabledMinutes_start: number[] = [];
+let array_disabledMinutes_end: number[] = [];
+let array_disabledSeconds_start: number[] = [];
+let array_disabledSeconds_end: number[] = [];
 
 let defaultTime1: [Date, Date] = [
   new Date(2000, 1, 1, 12, 0, 0),
   new Date(2000, 2, 1, 8, 0, 0),
-] // '12:00:00', '08:00:00'
+]; // '12:00:00', '08:00:00'
 
 // Event
 watch([textareain, checkbox1, textregex], () => {
@@ -116,7 +119,7 @@ watch([textareain, checkbox1, textregex], () => {
   if (checkbox1.value) {
     LogFilter();
   }
-})
+});
 
 // Function
 const LogFilterForBtn = () => {
@@ -128,23 +131,28 @@ const LogFilterForBtn = () => {
   }).then(() => {
     LogFilter();
   })
-}
+};
 
 const LogFilter = () => {
   if (textregex.value !== '' && textareain.value !== '') {
-    textareaout.value = array_textareain.map(s => regex_string.test(s) ? s : null)
+    // filter date
+    let [string_start, string_end] = timepicker.value ?? [null, null];
+    let [date_start, date_end] = [moment(string_start), moment(string_end)];
+    textareaout.value = array_textareain.filter(f => CompareTime(moment(GetTime(f)), date_start) >= 0
+        && CompareTime(moment(GetTime(f)), date_end) <= 0)
+        .map(s => regex_string.test(s) ? s : null)
         //.map(s => s.includes(textregex.value) ? s : null)
-        .filter(f => f != null)
+        .filter(f => f !== null)
         .join('\n');
   } else {
     textareaout.value = textareain.value;
   }
   IsLoading.value = false;
-}
+};
 
 const MappingData = () => {
   array_textareain = textareain.value
-      .split('\n');
+      .split('\n').filter(f => f !== "");
 
   // Date
   if (array_textareain.length > 0) {
@@ -152,17 +160,19 @@ const MappingData = () => {
     let string_last: string = _.last(array_textareain) ?? "";
     let date_start = moment(GetTime(string_first));
     let date_end = moment(GetTime(string_last));
+    array_disabledHours = makeRange(0, date_start.hour() - 1).concat(makeRange(date_end.hour() + 1, 23));
+    array_disabledMinutes_start = makeRange(0, date_start.minute() - 1);
+    array_disabledMinutes_end = makeRange(date_end.minute() + 1, 59);
+    array_disabledSeconds_start = makeRange(0, date_start.second() - 1);
+    array_disabledSeconds_end = makeRange(date_end.second() + 1, 59);
     timepicker.value = [
       new Date(2000, 1, 1, date_start.hour(), date_start.minute(), date_start.second()),
       new Date(2000, 1, 1, date_end.hour(), date_end.minute(), date_end.second()),
     ];
-    disabledHours.value = () => {
-      return makeRange(0, date_start.hour() - 1).concat(makeRange(date_end.hour() + 1, 23));
-    }
   } else {
 
   }
-}
+};
 
 const GetTime = (s: string) => {
   let string_array: string[] = [];
@@ -173,7 +183,7 @@ const GetTime = (s: string) => {
   });
   string_array = string_array.filter(f => moment("2000-01-01 " + f).isValid());
   return "2000-01-01 " + _.last(string_array);
-}
+};
 
 // TimePicker
 const makeRange = (start: number, end: number) => {
@@ -182,20 +192,49 @@ const makeRange = (start: number, end: number) => {
     result.push(i)
   }
   return result
-}
+};
 
-// let disabledHours = () => {
-//   let array_number: number[] = [];
-//   return array_number;
-// }
+const disabledHours = () => {
+  return array_disabledHours;
+};
 
 const disabledMinutes = (hour: number) => {
-  return [];
-}
+  if (_.first(_.difference(kArrayHours, array_disabledHours)) === hour) {
+    return array_disabledMinutes_start;
+  } else if (_.last(_.difference(kArrayHours, array_disabledHours)) === hour) {
+    return array_disabledMinutes_end;
+  } else {
+    return [];
+  }
+};
 
-const disabledSeconds = (start: number, end: number) => {
-  return [];
-}
+const disabledSeconds = (hour: number, minute: number) => {
+  if (_.first(_.difference(kArrayHours, array_disabledHours)) === hour && _.first(_.difference(kArrayMinutes, array_disabledMinutes_start)) === minute) {
+    return array_disabledSeconds_start;
+  } else if (_.last(_.difference(kArrayHours, array_disabledHours)) === hour && _.first(_.difference(kArraySeconds, array_disabledMinutes_end)) === minute) {
+    return array_disabledSeconds_end;
+  } else {
+    return [];
+  }
+};
+
+const CompareTime = (time1: any, time2: any) => {
+  if (time1.hour() > time2.hour()) {
+    return 1;
+  } else if (time1.hour() < time2.hour()) {
+    return -1;
+  } else if (time1.minute() > time2.minute()) {
+    return 1;
+  } else if (time1.minute() < time2.minute()) {
+    return -1;
+  } else if (time1.second() > time2.second()) {
+    return 1;
+  } else if (time1.second() < time2.second()) {
+    return -1;
+  } else {
+    return 0;
+  }
+};
 </script>
 
 <style scoped>
