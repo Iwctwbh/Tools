1a6f956a0e578e5d401a1dc488f041273c464e5d
夜夜三倍速 <iwctwbh@gmail.com>
2023-05-09 17:31:17 +0800

    Update UUIDCreate

--- src/views/UUIDCreate.vue
+++ src/views/UUIDCreate.vue
@@ -173,6 +173,11 @@ const isCaseLower = ref<boolean>(false);
 const isHasNumebr = ref<boolean>(false);
 const isHasSpecialSymbols = ref<boolean>(false);
 
+const arrayNumber: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const arraySpecialSymbols: string[] = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?", "~", "`", "|"];
+const arrayLower: string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const arrayUpper: string[] = arrayLower.map((item: string) => item.toUpperCase());
+
 // Event
 const btnUUIDCreate = () => {
   let uuidArray: string[] = [];
