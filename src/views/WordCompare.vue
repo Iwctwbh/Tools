275318f88553c56cf1e90380868d64d2746ca344
Iwctwbh <Iwctwbh@gmail.com>
2024-04-28 15:51:01 +0800

    Update WordCompare diffJson

--- src/views/WordCompare.vue
+++ src/views/WordCompare.vue
@@ -2,30 +2,47 @@
   <h1 class="title">
     文本对比 WordCompare
   </h1>
-  <el-row>
+  <el-row style="max-height: 80%;">
+    <el-col>
+      <el-radio-group
+        v-model="showWay"
+      >
+        <el-radio label="diffLines" size="large">line</el-radio>
+        <el-radio label="diffWords" size="large">word</el-radio>
+        <el-radio label="diffChars" size="large">char</el-radio>
+        <el-radio label="diffJson" size="large">json</el-radio>
+        <el-radio label="diffJsonWithSort" size="large">jsonWithSort</el-radio>
+        <el-radio label="diffJsonWithoutValue" size="large">jsonWithoutValue</el-radio>
+        <el-radio label="diffCss" size="large">css</el-radio>
+        <el-radio label="diffSentences" size="large">sentence</el-radio>
+        <el-radio label="diffTrimmedLines" size="large">trimmedLine</el-radio>
+        <el-radio label="diffWordsWithSpace" size="large">wordWithSpace</el-radio>
+      </el-radio-group>
+    </el-col>
     <el-col :lg="8">
       <div>
         <el-input
-            id="textareaIn"
-            v-model="textareaIn0"
-            :autosize="{ minRows: 15, maxRows: 30 }"
-            placeholder="Please input text"
-            type="textarea"
+          id="textareaIn"
+          v-model="textareaIn0"
+          :autosize="{ minRows: 30, maxRows: 30 }"
+          placeholder="Please input text"
+          type="textarea"
         />
       </div>
     </el-col>
     <el-col :lg="8">
       <el-input
-          id="textareaOut"
-          v-model="textareaIn1"
-          :autosize="{ minRows: 15, maxRows: 30 }"
-          placeholder="Please input text"
-          type="textarea"
+        id="textareaOut"
+        v-model="textareaIn1"
+        :autosize="{ minRows: 30, maxRows: 30 }"
+        placeholder="Please input text"
+        type="textarea"
       />
     </el-col>
 
     <el-col :lg="8">
-      <div id="divDiffOut" style="min-height: 325px; border: #dcdfe6 solid 1px; padding: 5px 11px;"></div>
+      <pre id="preDiffOut"
+           style="overflow:auto; min-height: 630px; max-height: 630px; border: #dcdfe6 solid 1px; padding: 5px 11px; margin: 0;"></pre>
     </el-col>
   </el-row>
 
@@ -39,18 +56,22 @@
 </template>
 
 <style>
-#divDiffOut span {
+#preDiffOut span {
   overflow-wrap: break-word;
 }
 
-#divDiffOut ins {
+#preDiffOut ins {
   overflow-wrap: break-word;
   color: green;
 }
 
-#divDiffOut del {
+#preDiffOut del {
   overflow-wrap: break-word;
   color: red;
+}
+
+textarea {
+  white-space: nowrap;
 }
 </style>
 
@@ -59,20 +80,78 @@
 
 // Init
 import {ref, watch} from "vue";
-import {diffChars} from "diff";
+import {
+  diffChars,
+  diffLines,
+  diffJson,
+  diffWords,
+  diffCss,
+  diffSentences,
+  diffTrimmedLines,
+  diffWordsWithSpace,
+  Change
+} from "diff";
 
 let sloth: any = {}; // 是否使用命名空间？
 const textareaIn0 = ref<string>("");
 const textareaIn1 = ref<string>("");
+const showWay = ref<string>("diffLines");
 
 let arrDiffOut: string[] = [];
 
 // Event
 // 监听textareaIn0, textareaIn1
-watch([textareaIn0, textareaIn1], (): void => {
+watch([textareaIn0, textareaIn1, showWay], (): void => {
   arrDiffOut = [];
-  let arrDiff = diffChars(textareaIn0.value, textareaIn1.value);
-  arrDiff.forEach(f => {
+  let arrDiff: Change[] | undefined;
+  switch (showWay.value) {
+    case "diffLines":
+      arrDiff = diffLines(textareaIn0.value, textareaIn1.value);
+      break;
+    case "diffWords":
+      arrDiff = diffWords(textareaIn0.value, textareaIn1.value);
+      break;
+    case "diffChars":
+      arrDiff = diffChars(textareaIn0.value, textareaIn1.value);
+      break;
+    case "diffJson":
+      arrDiff = diffJson(textareaIn0.value, textareaIn1.value);
+      break;
+    case "diffJsonWithSort":
+      try {
+        let text0: string = JSON.stringify(JSON.parse(textareaIn0.value), (obj, value) => typeof value !== "object" ? value : sortJSON(value), 4);
+        let text1: string = JSON.stringify(JSON.parse(textareaIn1.value), (obj, value) => typeof value !== "object" ? value : sortJSON(value), 4);
+        arrDiff = diffJson(text0, text1);
+      } catch {
+        arrDiff = diffJson(textareaIn0.value, textareaIn1.value);
+      }
+      break;
+    case "diffJsonWithoutValue":
+      try {
+        let text0: string = JSON.stringify(JSON.parse(textareaIn0.value),
+          (obj, value) => typeof value !== "object" ? "" : Array.isArray(value) ? value : sortJSON(value), 4);
+        let text1: string = JSON.stringify(JSON.parse(textareaIn1.value),
+          (obj, value) => typeof value !== "object" ? "" : Array.isArray(value) ? value : sortJSON(value), 4);
+        // let text1: string = JSON.stringify(JSON.parse(textareaIn1.value), (obj, value) => typeof value !== "object" || Array.isArray(value) ? "" : value, 4);
+        arrDiff = diffJson(text0, text1);
+      } catch {
+        arrDiff = diffJson(textareaIn0.value, textareaIn1.value);
+      }
+      break;
+    case "diffCss":
+      arrDiff = diffCss(textareaIn0.value, textareaIn1.value);
+      break;
+    case "diffSentences":
+      arrDiff = diffSentences(textareaIn0.value, textareaIn1.value);
+      break;
+    case "diffTrimmedLines":
+      arrDiff = diffTrimmedLines(textareaIn0.value, textareaIn1.value);
+      break;
+    case "diffWordsWithSpace":
+      arrDiff = diffWordsWithSpace(textareaIn0.value, textareaIn1.value);
+      break;
+  }
+  arrDiff?.forEach(f => {
     if (f.removed) {
       arrDiffOut.push("<del title=\"删除的部分\">" + f.value + "</del>");
     } else if (f.added) {
@@ -82,9 +161,29 @@ watch([textareaIn0, textareaIn1], (): void => {
       arrDiffOut.push("<span title=\"没有改动的部分\">" + f.value + "</span>");
     }
   });
-  let elementDiffOut = document.getElementById("divDiffOut");
+  let elementDiffOut = document.getElementById("preDiffOut");
   if (elementDiffOut) {
     elementDiffOut.innerHTML = arrDiffOut.join("");
   }
-});
+}); // 监听textareaIn0, textareaIn1
+
+// Function
+// sortJSON
+const sortJSON = (json: { [key: string]: any }): { [key: string]: any } => {
+  const sorted: { [key: string]: any } = {};
+  Object.keys(json).sort().forEach(key => {
+    sorted[key] = json[key];
+  });
+  return sorted;
+}; // sortJSON
+
+// sortByValue
+interface JsonItem {
+  value: number;
+  [key: string]: any;
+}
+
+const sortByJsonValue = (jsonArray: JsonItem[]): JsonItem[] => {
+  return jsonArray.sort((a, b) => a.value - b.value);
+};// sortByValue
 </script>
